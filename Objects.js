82fec720fbe75772f29bcf5bcf7f54c7c9dbab59
Wendy Bartos <wendyebartos@gmail.com>
2019-10-23 09:23:30 -0700

    added this and delete

--- Objects.js
+++ Objects.js
@@ -1,3 +1,4 @@
+'use strict';
 /*function createMyObject() {
   return {
     foo: 'bar',
@@ -11,16 +12,45 @@
 
 console.log(createMyObject()); */
 
-function updateObject(obj) {
-  obj.foo = 'foo';
-  obj.bar = 'bar';
-  obj.bizz = 'bizz';
-  obj.bang = 'bang';
-  return obj;
-}
+// function updateObject(obj) {
+//   obj.foo = 'foo';
+//   obj.bar = 'bar';
+//   obj.bizz = 'bizz';
+//   obj.bang = 'bang';
+//   return obj;
+// }
 
-const test = {
-  name: 'practice'
-};
+// const test = {
+//   name: 'practice'
+// };
 
-console.log(updateObject(test));
+// console.log(updateObject(test));
+
+// function personMaker() {
+//   return {
+//     firstName: 'Paul',
+//     lastName: 'Jones',
+//     // replace `null` with a function that uses self reference to return
+//     // full name
+//     fullName() {
+//       return `${this.firstName} ${this.lastName}`;
+//     }
+//   };
+// };
+// console.log(personMaker().fullName());
+
+//deleting keys
+// function keyDeleter(obj) {
+//   delete obj.foo;
+//   delete obj.bar;
+//   return obj;
+// }
+
+// const sampleObj = {
+//   foo: 'foo',
+//   bar: 'bar',
+//   bizz: 'bizz',
+//   bang: 'bang',
+// };
+// const result = keyDeleter(sampleObj);
+// console.log(result);
