81619ea7e70e3191edf72babb6033e638c6cfad4
Kevin Moore <moore.kevin.p@gmail.com>
2019-10-23 11:53:06 -0400

    Object drills 1 and 2

---
+++ Objects.js
@@ -0,0 +1,26 @@
+/*function createMyObject() {
+  return {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello() {
+      return 'Hello';
+    }
+  };
+}
+
+console.log(createMyObject()); */
+
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+}
+
+const test = {
+  name: 'practice'
+};
+
+console.log(updateObject(test));
